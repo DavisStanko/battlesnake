5a8154db09c652ac092033d8f4f04231e2cbabaa
davis stanko <davisbvstanko@gmail.com>
2022-11-05 16:52:42 -0400

    import hotfix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from server import run_server
 import functions
-# import functions2
 
 def main():
   run_server({
@@ -8,11 +7,6 @@ def main():
       "start": functions.start,
       "move": functions.move,
       "end": functions.end
-
-      # "info": functions2.info,
-      # "start": functions2.start,
-      # "move": functions2.move,
-      # "end": functions2.end
   })
 
 
