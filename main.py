29d3d7e0b453b4c768a14a8f38080a8610529192
davis stanko <davisbvstanko@gmail.com>
2023-05-09 11:55:35 -0400

    indentation

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from server import run_server
 import functions
 
+
 def main():
-  run_server({
-      "info": functions.info,
-      "start": functions.start,
-      "move": functions.move,
-      "end": functions.end
-  })
+    run_server({
+        "info": functions.info,
+        "start": functions.start,
+        "move": functions.move,
+        "end": functions.end
+    })
 
 
 if __name__ == "__main__":
-  main()
+    main()
